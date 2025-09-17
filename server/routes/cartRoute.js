eb5f3a4c952e007fed5caa3bdb6d34b6c9ada3ad
Sandesh Dahal <sandeshdahal3547@gmail.com>
2025-09-17 07:32:56 +0545

    importing mongoose in cartRouter

--- server/routes/cartRoute.js
+++ server/routes/cartRoute.js
@@ -1,5 +1,6 @@
 import { updateCart } from "../controllers/cartController.js";
 import authUser from "../middlewares/authUser.js";
+import mongoose from "mongoose";
 
 
 const cartRouter = mongoose.Router();
