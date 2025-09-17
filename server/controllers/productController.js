b36d1e2b6de11359248209e6e6362e12374e6117
Sandesh Dahal <sandeshdahal3547@gmail.com>
2025-09-17 07:09:21 +0545

    Add product controller function is completed.

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,7 +1,23 @@
-
+import  {v2 as cloudinary} from "cloudinary";
+import Product from "../models/product.js";
 //Add product : api/product/add
 export const addProduct = async (req, res) => {
+  try {
+    let productData = JSON.parse(req.body.productData);
 
+    const images = req.files;
+
+    let imgesUrl = await Promise.all(images.map(async (item)=>{
+      let result = await cloudinary.uploader.upload(item.path, {resource_type: 'image'});
+      return result.secure_url;
+    }))
+    await Product.create({...productData, image: imagesUrl});
+
+    res.json({success: true, message: "Product added successfully"});
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message: error.message});
+  }
 }
 
 //Get Product : /api/product/list
