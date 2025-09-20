3c23773d172e95d1df6c6bbe9946a136e59fc2ad
Sandesh Dahal <sandeshdahal3547@gmail.com>
2025-09-20 11:26:13 +0545

    created App context to use in other components.

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, use, useContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+  const value = { navigate, user, setUser, isSeller, setIsSeller };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
