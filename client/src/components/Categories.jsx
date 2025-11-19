74d3d54fe17517093d868a502933ee87976fd4a7
Sandesh Dahal <sandeshdahal3547@gmail.com>
2025-11-19 19:18:36 +0545

    Designing the categories section.

---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div>
+      <p>
+        Categories
+      </p>
+      <div>
+        <div>
+          <img src={assets.box_icon} alt="" />
+          <p>fruit</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Categories
